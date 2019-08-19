d4eac8cc884609c260c9615cee6d8b24b634fe59
Andre Brait <andre.brait@getbux.com>
2019-08-19 18:41:38 +0200

    Much better performance on the Suspect ActorRef calls inspection

--- src/org/elasticsoftware/elasticactors/plugin/SuspectActorRefCallsInspection.java
+++ src/org/elasticsoftware/elasticactors/plugin/SuspectActorRefCallsInspection.java
@@ -3,7 +3,6 @@ package org.elasticsoftware.elasticactors.plugin;
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.lang.jvm.JvmModifier;
-import com.intellij.psi.CommonClassNames;
 import com.intellij.psi.JavaElementVisitor;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiClassType.ClassResolveResult;
@@ -17,11 +16,10 @@ import com.intellij.psi.search.searches.ClassInheritorsSearch;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.Set;
+import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import static com.intellij.psi.util.PsiTypesUtil.getPsiClass;
 import static com.intellij.psi.util.PsiUtil.resolveGenericsClassInType;
@@ -30,22 +28,6 @@ import static org.elasticsoftware.elasticactors.Utils.isActorRefMethod;
 import static org.elasticsoftware.elasticactors.Utils.isMessage;
 
 public class SuspectActorRefCallsInspection extends AbstractBaseJavaLocalInspectionTool {
-
-    private static final Set<String> ALL_COMMON_CLASSES =
-            Arrays.stream(CommonClassNames.class.getDeclaredFields())
-                    .filter(f -> f.getType().equals(String.class))
-                    .map(f -> {
-                        try {
-                            return f.get(null);
-                        } catch (IllegalAccessException e) {
-                            return null;
-                        }
-                    }).filter(Objects::nonNull)
-                    .map(String.class::cast)
-                    .filter(s -> s.contains("."))
-                    .filter(s -> !CommonClassNames.DEFAULT_PACKAGE.equals(s))
-                    .filter(s -> !CommonClassNames.CLASS_FILE_EXTENSION.equals(s))
-                    .collect(Collectors.toSet());
 
     @NotNull
     @Override
@@ -109,48 +91,92 @@ public class SuspectActorRefCallsInspection extends AbstractBaseJavaLocalInspect
             @NotNull Supplier<PsiClass> classSupplier) {
         PsiClass argClass = classSupplier.get();
         if (argClass == null) {
-            holder.registerProblem(
-                    argument,
-                    "Unexpected argument type: " +
-                            (type != null
-                                    ? type.getCanonicalText()
-                                    : "UNKNOWN"));
+            registerProblemForType(holder, argument, type);
             return;
         }
-        if (isNotMessage(argClass)) {
-            if (isConcrete(argClass)) {
-                holder.registerProblem(
-                        argument,
-                        "Argument should be of a type annotated with @Message");
+        if (isJavaCorePackage(argClass)) {
+            registerGenericProblem(holder, argument);
+        } else if (isConcrete(argClass)) {
+            if (isFinal(argClass)) {
+                if (!isMessage(argClass)) {
+                    registerGenericProblem(holder, argument);
+                }
+            } else {
+                registerProblemForInheritors(holder, argument, argClass, false);
             }
-            if (!isJavaCorePackage(argClass)
-                    && (!isConcrete(argClass) || !argClass.hasModifier(JvmModifier.FINAL))) {
-                ClassInheritorsSearch.search(argClass).findAll().stream()
-                        .filter(SuspectActorRefCallsInspection::isConcrete)
-                        .filter(SuspectActorRefCallsInspection::isNotMessage)
-                        .limit(5)
-                        .forEach(psiClass -> holder.registerProblem(
-                                argument,
-                                "Found possible inheritor not annotated with @Message: "
-                                        + psiClass.getQualifiedName()));
-            }
+        } else {
+            registerProblemForInheritors(holder, argument, argClass, true);
         }
     }
 
-    private static boolean isJavaCorePackage(PsiClass psiClass) {
-        String qualifiedName = psiClass.getQualifiedName();
-        return qualifiedName == null
-                || qualifiedName.startsWith("java.")
-                || ALL_COMMON_CLASSES.contains(qualifiedName);
+    private static void registerProblemForInheritors(
+            @NotNull ProblemsHolder holder,
+            @NotNull PsiExpression argument,
+            @NotNull PsiClass argClass,
+            boolean registerIfHasNoInheritors) {
+        List<PsiClass> foundClasses = new ArrayList<>(5);
+        boolean[] found = {false};
+        ClassInheritorsSearch.search(argClass).forEach(inheritor -> {
+            found[0] = true;
+            if (isConcrete(inheritor) && !isMessage(inheritor)) {
+                foundClasses.add(inheritor);
+            }
+            return foundClasses.size() < 5;
+        });
+        if (foundClasses.isEmpty()) {
+            if (registerIfHasNoInheritors && !found[0]) {
+                registerGenericProblem(holder, argument);
+            }
+        } else {
+            foundClasses
+                    .forEach(inheritor -> registerProblemForInheritor(holder, argument, inheritor));
+        }
     }
 
-    private static boolean isConcrete(@Nullable PsiClass argClass) {
-        return argClass != null
-                && !argClass.isInterface()
+    private static void registerProblemForType(
+            @NotNull ProblemsHolder holder,
+            @NotNull PsiExpression argument,
+            @Nullable PsiType type) {
+        holder.registerProblem(argument, "Unexpected argument type: "
+                + (type != null ? type.getCanonicalText() : "UNKNOWN"));
+    }
+
+    private static void registerGenericProblem(
+            @NotNull ProblemsHolder holder,
+            @NotNull PsiExpression argument) {
+        holder.registerProblem(
+                argument,
+                "Argument should be of a type annotated with @Message");
+    }
+
+    private static void registerProblemForInheritor(
+            @NotNull ProblemsHolder holder,
+            @NotNull PsiExpression argument,
+            @NotNull PsiClass psiClass) {
+        holder.registerProblem(
+                argument,
+                "Found possible inheritor not annotated with @Message: "
+                        + psiClass.getQualifiedName());
+    }
+
+    private static boolean isFinal(@NotNull PsiClass psiClass) {
+        return psiClass.hasModifier(JvmModifier.FINAL) || !hasInheritableConstructors(psiClass);
+    }
+
+    private static boolean hasInheritableConstructors(@NotNull PsiClass psiClass) {
+        PsiMethod[] constructors = psiClass.getConstructors();
+        return constructors.length == 0
+                || Arrays.stream(constructors).anyMatch(c -> !c.hasModifier(JvmModifier.PRIVATE));
+    }
+
+    private static boolean isJavaCorePackage(@NotNull PsiClass psiClass) {
+        String qualifiedName = psiClass.getQualifiedName();
+        return qualifiedName == null || qualifiedName.startsWith("java.");
+    }
+
+    private static boolean isConcrete(@NotNull PsiClass argClass) {
+        return !argClass.isInterface()
                 && !argClass.hasModifier(JvmModifier.ABSTRACT);
     }
 
-    private static boolean isNotMessage(@Nullable PsiClass argClass) {
-        return !isMessage(argClass);
-    }
 }
