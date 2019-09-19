216b42009cd828fcc7edcc27a84af85709581ab8
Andre Brait <andre.brait@getbux.com>
2019-09-19 16:18:45 +0200

    Small fix for inspection when a class has no fields

--- src/org/elasticsoftware/elasticactors/plugin/IncorrectMessageMutabilityInspection.java
+++ src/org/elasticsoftware/elasticactors/plugin/IncorrectMessageMutabilityInspection.java
@@ -12,7 +12,6 @@ import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiIdentifier;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +19,8 @@ import static com.intellij.lang.jvm.JvmModifier.FINAL;
 import static com.intellij.lang.jvm.JvmModifier.STATIC;
 import static org.elasticsoftware.elasticactors.Utils.MESSAGE_ANNOTATION_CLASS;
 import static org.elasticsoftware.elasticactors.Utils.isMessage;
+
+import static java.util.Arrays.stream;
 
 public class IncorrectMessageMutabilityInspection extends AbstractBaseJavaLocalInspectionTool {
 
@@ -39,20 +40,19 @@ public class IncorrectMessageMutabilityInspection extends AbstractBaseJavaLocalI
                             PsiAnnotationMemberValue immutable =
                                     message.findAttributeValue("immutable");
                             if (immutable != null) {
-                                List<PsiField> nonStaticFields =
-                                        Arrays.stream(aClass.getAllFields())
-                                                .filter(PsiElement::isValid)
-                                                .filter(psiField -> !psiField.hasModifier(STATIC))
-                                                .filter(psiField -> psiField.getContainingClass() != null)
-                                                .filter(psiField -> !Throwable.class.getName().equals(psiField.getContainingClass().getQualifiedName()))
-                                                .collect(Collectors.toList());
-                                if (!nonStaticFields.isEmpty()) {
-                                    validateNonStaticFields(
-                                            nameIdentifier,
-                                            immutable,
-                                            nonStaticFields,
-                                            holder);
-                                }
+                                List<PsiField> nonStaticFields = stream(aClass.getAllFields())
+                                        .filter(PsiElement::isValid)
+                                        .filter(psiField -> !psiField.hasModifier(STATIC))
+                                        .filter(psiField -> psiField.getContainingClass() != null)
+                                        .filter(psiField -> !Throwable.class.getName()
+                                                .equals(psiField.getContainingClass()
+                                                        .getQualifiedName()))
+                                        .collect(Collectors.toList());
+                                validateNonStaticFields(
+                                        nameIdentifier,
+                                        immutable,
+                                        nonStaticFields,
+                                        holder);
                             }
                         }
                     }
@@ -64,19 +64,21 @@ public class IncorrectMessageMutabilityInspection extends AbstractBaseJavaLocalI
     private static void validateNonStaticFields(
             PsiIdentifier nameIdentifier,
             PsiAnnotationMemberValue immutable,
-            List<PsiField> nonStaticFields,
+            List<PsiField> fields,
             @NotNull ProblemsHolder holder) {
         if (Boolean.parseBoolean(immutable.getText())) {
-            if (nonStaticFields.stream().anyMatch(psiField -> !psiField.hasModifier(FINAL))) {
+            if (!fields.isEmpty()
+                    && fields.stream().anyMatch(psiField -> !psiField.hasModifier(FINAL))) {
                 holder.registerProblem(
                         nameIdentifier,
-                        "@Message-annotated class marked as immutable, but has non-final fields");
+                        "@Message-annotated class marked as immutable has non-final fields");
             }
         } else {
-            if (nonStaticFields.stream().allMatch(psiField -> psiField.hasModifier(FINAL))) {
+            if (fields.isEmpty()
+                    || fields.stream().allMatch(psiField -> psiField.hasModifier(FINAL))) {
                 holder.registerProblem(
                         nameIdentifier,
-                        "@Message-annotated class marked as mutable, but only has final fields");
+                        "@Message-annotated class marked as mutable doesn't have non-final fields");
             }
         }
     }
